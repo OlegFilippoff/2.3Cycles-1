ff5561adf83c0154a80891ac20830501d9555454
Oleg Filippov <5157175@bk.ru>
2022-01-18 15:30:27 +0300

    Main deleted+SQRServiceReowrked

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -5,13 +5,11 @@ public class SQRService {
     public int rangeNum(int lowerLimit, int upperLimit) {
         int counter = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= lowerLimit) {
-                if (i * i <= upperLimit) {
+            if (i * i >= lowerLimit && i * i <= upperLimit) {
                     counter = counter + 1;
                 }
             }
-        }
-        System.out.println("Количество удовлетворяющих чисел равно " + counter);
         return counter;
     }
 }
+
